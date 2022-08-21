a0a7a1b06c76fb978a2ee1f64defcfac7222078d
iceg <2327259688@qq.com>
2022-08-21 11:27:55 +0800

    feat: 支持配置每页行数和行距；去除空格

--- src/main/java/com/thief/idea/YueDuService.java
+++ src/main/java/com/thief/idea/YueDuService.java
@@ -1,6 +1,7 @@
 package com.thief.idea;
 
 import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ObjectUtil;
 import com.thief.idea.client.yuedu.BookClient;
 import com.thief.idea.pojo.param.GetBookChapterListParam;
 import com.thief.idea.pojo.param.GetBookContentParam;
@@ -10,18 +11,27 @@ import com.thief.idea.pojo.vo.BookChapter;
 import java.util.*;
 
 public class YueDuService {
-    private Book book;
+    private Book book = null;
 
-    private Integer lineIndex;
-    private Integer chapterIndex;
+    private Integer lineIndex = 0;
+    private Integer chapterIndex = 0;
 
     private List<BookChapter> bookChapterList;
     private String bookContent = "";
 
-    private Map<Integer, List<String>> bookContentMap = new HashMap<>();
+    private final Map<Integer, List<String>> bookContentMap = new HashMap<>();
     private List<String> bookContentLineList = Collections.emptyList();
 
-    private PersistentState persistentState;
+    private PersistentState persistentState = null;
+    /**
+     * 读取每页行数设置
+     **/
+    private Integer lineCount = 1;
+
+    /**
+     * 读取行距设置
+     **/
+    private Integer lineSpace = 0;
 
     public Integer getCurrentLine() {
         return lineIndex + 1;
@@ -32,11 +42,29 @@ public class YueDuService {
     }
 
     public YueDuService(PersistentState persistentState) {
-        Book book = new Book();
-        book.setBookUrl(persistentState.getBookPathText());
-        book.setDurChapterIndex(Integer.valueOf(persistentState.getChapterIndex()));
-        book.setDurChapterPos(Integer.valueOf(persistentState.getLineIndex()));
         this.persistentState = persistentState;
+        this.lineCount = Integer.parseInt(persistentState.getLineCount());
+        this.lineSpace = Integer.parseInt(persistentState.getLineSpace());
+
+        init(persistentState.getBookPathText(), persistentState.getChapterIndex(), persistentState.getLineIndex());
+    }
+
+    public YueDuService(String bookUrl, String chapterIndex, String lineIndex) {
+        init(bookUrl, chapterIndex, lineIndex);
+    }
+
+    public YueDuService(String bookUrl, String chapterIndex, String lineIndex, String lineCount, String lineSpace) {
+        this.lineCount = Integer.valueOf(lineCount);
+        this.lineSpace = Integer.valueOf(lineSpace);
+
+        init(bookUrl, chapterIndex, lineIndex);
+    }
+
+    private void init(String bookUrl, String chapterIndex, String lineIndex) {
+        Book book = new Book();
+        book.setBookUrl(bookUrl);
+        book.setDurChapterIndex(Integer.valueOf(chapterIndex));
+        book.setDurChapterPos(Integer.valueOf(lineIndex));
         this.chapterIndex = book.getDurChapterIndex();
         this.lineIndex = book.getDurChapterPos();
         this.book = book;
@@ -48,13 +76,15 @@ public class YueDuService {
             System.out.println("获取目录为空");
         } else {
             this.book.setTotalChapterNum(bookChapterList.size());
-            loadBookChapter(chapterIndex);
+            loadBookChapter(this.chapterIndex);
         }
     }
 
     private void saveProcess() {
-        persistentState.setChapterIndex(chapterIndex.toString());
-        persistentState.setLineIndex(lineIndex.toString());
+        if (!ObjectUtil.isEmpty(persistentState)) {
+            persistentState.setChapterIndex(chapterIndex.toString());
+            persistentState.setLineIndex(lineIndex.toString());
+        }
     }
 
 
@@ -145,8 +175,48 @@ public class YueDuService {
     }
 
     private void refreshBookContentLinkList() {
-        bookContent = bookContent.replaceAll("\\u00A0+", "");
-        bookContentLineList = new ArrayList<>(Arrays.stream(bookContent.split("\\n")).map(String::trim).filter(x -> !x.isBlank()).toList());
+        bookContent = bookContent
+                .replaceAll("\\u00a0+", "")
+                .replaceAll("\\u2003+", "")
+                .replaceAll("\\u2002+", "");
+        List<String> tmpList = new ArrayList<>(
+                Arrays.stream(bookContent.split("\\n"))
+                        .map(String::trim)
+                        .map(x -> (x.replaceAll("^[　*| *| *|//*]*", "").replaceAll("[　*| *| *|//*]*$", "")))
+                        .filter(x -> !x.isBlank())
+                        .toList()
+        );
+        bookContentLineList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i < tmpList.size() + 1; i++) {
+            if (i % lineCount == 0) {
+                stringBuilder.append(tmpList.get(i - 1));
+                bookContentLineList.add(stringBuilder.toString());
+                stringBuilder.setLength(0);
+            } else {
+                stringBuilder.append(tmpList.get(i - 1));
+                stringBuilder.append("\n".repeat(Math.max(0, lineSpace + 1)));
+            }
+        }
+
         bookContentLineList.set(0, "【" + bookChapterList.get(chapterIndex).getTitle() + "】" + bookContentLineList.get(0));
+    }
+
+    public static void main(String[] args) {
+        String bookUrl = "https://www.biqugexx.com/120_120641/";
+        String chapterIndex = "0";
+        String lineIndex = "0";
+        String lineCount = "2";
+        String lineSpace = "1";
+        YueDuService yueDuService = new YueDuService(bookUrl, chapterIndex, lineIndex, lineCount, lineSpace);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(yueDuService.refreshBookContentLine());
+
+        for (int i = 0; i < 10; i++) {
+            stringBuilder.append("\\n");
+            stringBuilder.append(yueDuService.getBookContentNextLine());
+        }
+
+        System.out.println(stringBuilder);
     }
 }
